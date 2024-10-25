24e6f60cd80a2e457155b33fc5b6e0189a0f9dd7
abhay <abhaysaini364@gmail.com>
2024-10-25 17:16:20 +0530

    first coomit

---
+++ Table_5.java
@@ -0,0 +1,16 @@
+public class Table_5{
+
+public static void main(String[]args){
+
+
+int n=5;
+
+System.out.println("multiplication table of 5 is..........");
+
+for(int i=1;i<=10;i++){
+
+System.out.println("5X"+i+"="+5*i);
+
+}
+}
+}
